47ea2841093e851e47307a6b4346d640c25d8c94
Peerzaad <peerzaad@docquik.com>
2025-06-16 13:12:43 +0530

    error handling

--- routing-demo/src/app/blog/page.tsx
+++ routing-demo/src/app/blog/page.tsx
@@ -1,4 +1,5 @@
 import {Metadata} from 'next'
+import { resolve } from 'path'
 import React from 'react'
 
 export const metadata:Metadata = {
@@ -9,7 +10,12 @@ export const metadata:Metadata = {
 }
 
 
-const Blog = () => {
+const Blog = async() => {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("intentional delay")
+    }, 2000)
+  })
   return (
   <h1>Blog</h1>
   )
--- routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,13 +1,25 @@
 import React from 'react'
-import { notFound } from 'next/navigation';
+import { notFound, redirect } from 'next/navigation';
+
+
+function getRandomInt(count:number){
+  return Math.floor(Math.random() * count);
+}
+
+
 const ProductReview = async({
     params
 }: {
     params: Promise<{productId: string; reviewId: string}>
 }) => {
+  const random = getRandomInt(2);
+  if(random === 1){
+    throw new Error("Error loading review")
+  }
     const {productId, reviewId} = await params;
     if(parseInt(reviewId)> 1000){
-      notFound();
+      // notFound();
+      redirect("/products")
     }
   return (
 <h1>Review {reviewId} for product {productId}</h1>
