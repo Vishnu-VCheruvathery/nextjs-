1224e87554015a7d062c6fabc5c179ea5cb23174
Peerzaad <peerzaad@docquik.com>
2025-06-17 12:52:26 +0530

    tailwind added

--- routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -2,9 +2,6 @@ import React from 'react'
 import { notFound, redirect } from 'next/navigation';
 
 
-function getRandomInt(count:number){
-  return Math.floor(Math.random() * count);
-}
 
 
 const ProductReview = async({
@@ -12,10 +9,7 @@ const ProductReview = async({
 }: {
     params: Promise<{productId: string; reviewId: string}>
 }) => {
-  const random = getRandomInt(2);
-  if(random === 1){
-    throw new Error("Error loading review")
-  }
+
     const {productId, reviewId} = await params;
     if(parseInt(reviewId)> 1000){
       // notFound();
